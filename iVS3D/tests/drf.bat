4f2ee101768c7016362369ecf00aba89b329f83f
Dominik Wuest <dominik.wuest@student.kit.edu>
2022-01-18 10:13:00 +0100

    updated google drive download link for testresources

--- iVS3D/tests/drf.bat
+++ iVS3D/tests/drf.bat
@@ -2,5 +2,5 @@
 ECHO Downloading test resources...
 SET "outpath=%~dp0"
 
-powershell.exe -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://drive.google.com/uc?export=download&id=1ZCLGsmmX6rlcTYSoQlV8LVll-kWSCAgN' -OutFile %outpath%/test.zip; Expand-Archive %outpath%/test.zip -DestinationPath %outpath%/testresources; Remove-Item %outpath%/test.zip;"
+powershell.exe -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://drive.google.com/uc?export=download&id=1tR3vq25R-qqbgzOoyMUvKVnEPJm7UE6o' -OutFile %outpath%/test.zip; Expand-Archive %outpath%/test.zip -DestinationPath %outpath%/testresources; Remove-Item %outpath%/test.zip;"
 ECHO Finished downloading...
--- iVS3D/tests/drf.sh
+++ iVS3D/tests/drf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget "https://drive.google.com/uc?export=download&id=1ZCLGsmmX6rlcTYSoQlV8LVll-kWSCAgN" -O test.zip
+wget "https://drive.google.com/uc?export=download&id=1tR3vq25R-qqbgzOoyMUvKVnEPJm7UE6o" -O test.zip
 unzip -o test.zip -d testresources
 rm test.zip
