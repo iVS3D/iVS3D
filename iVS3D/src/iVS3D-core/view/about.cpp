ef6481358e7113b22df29bf36cc5252851abf8f5
Dominik Wuest <dominik.wuest@iosb.fraunhofer.de>
2022-03-16 15:01:32 -0700

    added following flags to pass version and date to build process: IVS3D_DAT for build date and IVS3D_VER for build version. can be passed to qmake via DEFINES

--- iVS3D/src/iVS3D-core/view/about.cpp
+++ iVS3D/src/iVS3D-core/view/about.cpp
@@ -1,11 +1,24 @@
 #include "about.h"
 #include "ui_about.h"
 
+#ifndef IVS3D_VER
+#define IVS3D_VER unknown
+#endif
+
+#ifndef IVS3D_DAT
+#define IVS3D_DAT unknown
+#endif
+
+#define QUOTE(str) _QUOTE(str)
+#define _QUOTE(str) #str
+
 About::About(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::About)
 {
     ui->setupUi(this);
+    ui->l_date->setText("build date: " + QString(QUOTE(IVS3D_DAT)));
+    ui->l_version->setText("build version: " + QString(QUOTE(IVS3D_VER)));
 }
 
 About::~About()
