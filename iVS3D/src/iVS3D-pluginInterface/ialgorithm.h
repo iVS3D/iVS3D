59913a62971a1f626e835cf93cde537601a66d08
Boitumelo Ruf <Boitumelo.Ruf@iosb.fraunhofer.de>
2023-10-24 12:49:48 +0200

    Added rpath ORIGIN to plugins

--- iVS3D/src/iVS3D-pluginInterface/ialgorithm.h
+++ iVS3D/src/iVS3D-pluginInterface/ialgorithm.h
@@ -19,7 +19,7 @@
  *
  * @brief The IAlgorithm interface is used to include plugin implementations for different sampling algorithms. As such the plugins need to present a display name
  * and have the possibility to display a custom QWidget to the user. The main task for a IAlgorithm plugin to select keyframes based on the given images. This is
- * performed in sampleImages. Images are accessible by the Reader object. The currently selected images are apssed as indices in imageList. This allows the plugin
+ * performed in sampleImages. Images are accessible by the Reader object. The currently selected images are passed as indices in imageList. This allows the plugin
  * to sample on the current image selection as well as include intermediate images in the computation. The plugin can write data to an QVariant in order to buffer them.
  * Buffered data is managed by the core application and is passed to the plugin on the next sampleImages call.
  *
@@ -46,7 +46,7 @@ public:
     virtual QWidget* getSettingsWidget(QWidget* parent) = 0;
 
     /**
-     * @brief getName returns the display name for the plugin. This name is pesented to the user.
+     * @brief getName returns the display name for the plugin. This name is presented to the user.
      * @return The name to display
      */
     virtual QString getName() const = 0;
@@ -54,9 +54,9 @@ public:
     /**
      * @brief sampleImages selects the keyframes from the given images. The computation is based on the images provided by the given Reader. The imageList
      * provides indices for the currently selected keyframes. This way the current selection can be taken in account in the calculation. Since the algorithm
-     * can be very timeconsuming, it is recommended to report progress to the given Progressable by invoking Progressable::slot_makeProgress. It should be
+     * can be very time consuming, it is recommended to report progress to the given Progressable by invoking Progressable::slot_makeProgress. It should be
      * possible to abort calculation by setting the value of stopped to @a true. Therefore it is necessary to repeatedly check its state. To speedup calculation,
-     * data can be buffered to accelerate the next call of sampleImages. On some systems the gpu can be utalized for acceleration as well, but only if useCuda is
+     * data can be buffered to accelerate the next call of sampleImages. On some systems the gpu can be utilized for acceleration as well, but only if useCuda is
      * @a true. See following example implementation for sampleImages:
      *
      * @code
@@ -94,19 +94,19 @@ public:
      *  return keyframes;
      * @endcode
      *
-     * @param imageList Index list of images to compute, but indices inbetween can be used for computation (see Blur)
+     * @param imageList Index list of images to compute, but indices in between can be used for computation (see Blur)
      * @param receiver The Progressable to invoke to report progress
-     * @param stopped Flag @a true if the computation should abort, @a fals if it should continue
+     * @param stopped Flag @a true if the computation should abort, @a false if it should continue
      * @param useCuda @a true if cv::cuda can be used
-     * @param logFile can be used to protocoll progress or problems
-     * @return The indices of the seletced keyframes
+     * @param logFile can be used to protocol progress or problems
+     * @return The indices of the selected keyframes
      *
      * @see Rotation, Blur
      */
     virtual std::vector<uint> sampleImages(const std::vector<uint> &imageList, Progressable* receiver, volatile bool* stopped, bool useCuda, LogFileParent *logFile) = 0;
 
     /**
-     * @brief initialize the the IAlgorithm and the settings widget with plusible values from the Reader.
+     * @brief initialize the the IAlgorithm and the settings widget with plausible values from the Reader.
      * @param reader The reader with the images
      * @param buffer QVariant with the buffered data form last call to sampleImages
      * @param sigObj provides signals from the core application
