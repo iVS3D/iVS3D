c9390e177e619c4846e776bfbb326a19c769df6d
Dominik Wüst <dominik.wuest@web.de>
2024-01-23 15:28:21 +0100

    rebased geo-distance plugin without the map plugin

---
+++ iVS3D/src/iVS3D-geoDistancePlugin/translations/geodistance_en.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>GeoDistance</name>
+    <message>
+        <location filename="../geodistance.cpp" line="47"/>
+        <source>GeoDistance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geodistance.cpp" line="208"/>
+        <source>This plugin uses the geo location provided in the meta data to calculate the distance between images. An image is selected as a new keyframe, if the distance to the previous keyframe is greater than the specified threshold.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geodistance.cpp" line="219"/>
+        <source>Select distance in meter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geodistance.cpp" line="229"/>
+        <source>The minimum distance between two consecutive keyframes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geodistance.cpp" line="235"/>
+        <source>Include altitude in calculation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
