6bba99a54146876acc894d58097679685613cd1d
Dominik Wüst <dominik.wuest@iosb.fraunhofer.de>
2025-08-25 11:37:45 +0200

    refactor: streamline OpenCV dependency copying by using wildcard for DLLs

--- ci/make-upload-package.ps1
+++ ci/make-upload-package.ps1
@@ -48,12 +48,7 @@ foreach ($file in $files) {
 
 # add OpenCV dependencies
 Write-Host "`nCopying OpenCV dlls"
-Copy-Item -Path "${OCV_BIN}\opencv_world${OCV_VERSION}.dll" -Destination "."
-Copy-Item -Path "${OCV_BIN}\opencv_videoio_ffmpeg${OCV_VERSION}_64.dll" -Destination "."
-
-If($CUDA_VERSION){
-  Copy-Item -Path "${OCV_BIN}\opencv_img_hash${OCV_VERSION}.dll" -Destination "."
-}
+Copy-Item -Path "${OCV_BIN}\*.dll" -Destination "."
 
 # add MSVC runtime
 Write-Host "`nCopying MSVC ${MSVC_VERSION} runtime dlls"
