9a8b1f1dfcfe17229461c9cae94d94cf46d149e2
Dominic Zahn <zahndominic@web.de>
2025-09-12 10:21:41 +0200

    Removed sampling resolution of opt flow plugins

--- iVS3D/src/iVS3D-smoothCameraMovementPlugin/imagegatherercpu.cpp
+++ iVS3D/src/iVS3D-smoothCameraMovementPlugin/imagegatherercpu.cpp
@@ -1,21 +1,20 @@
 #include "imagegatherercpu.h"
 
-ImageGathererCpu::ImageGathererCpu(Reader *reader, double downSampleFactor, std::vector<uint> futureFrames)
-    : ImageGatherer(reader, downSampleFactor, futureFrames)
+ImageGathererCpu::ImageGathererCpu(Reader *reader, std::vector<uint> futureFrames)
+    : ImageGatherer(reader, futureFrames)
 {
 
 }
 
 cv::Mat ImageGathererCpu::gatherSingleImage(uint frameIdx)
 {
-    cv::Mat readMat, downMat, greyMat;
+    cv::Mat readMat, greyMat;
     readMat = m_reader->getPic(frameIdx);
 
     if (readMat.empty())
         return cv::Mat();
 
-    cv::resize(readMat, downMat, cv::Size(), m_reciprocalDownSampleFactor, m_reciprocalDownSampleFactor);
-    cv::cvtColor(downMat, greyMat, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(readMat, greyMat, cv::COLOR_BGR2GRAY);
 
     return greyMat;
 }
